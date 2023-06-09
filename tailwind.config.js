e57b7274f6b1ecac08c1ca755b0e3627d9cc1cf9
Lucas Vieira <ls6182315@gmail.com>
2023-06-09 14:03:31 +0000

    add new css sett

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -12,6 +12,29 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['Poppins', 'Bold','sans-serif'],
+      },
+      colors: {
+        black: {
+          50: '#000000',
+          67: '#333333'
+        },
+        orange: {
+          50: '#FF881F',
+          150: '#FA641E',
+          200: '#FF671F',
+        },
+        gray: {
+          50: '#FAFAFA',
+          75: '#F0F0F0',
+          87: '#999999',
+        },
+        white: {
+          50: '#FFFFFF',
+          100: '#00000000',
+        }
+      }
     },
   },
   plugins: [],
