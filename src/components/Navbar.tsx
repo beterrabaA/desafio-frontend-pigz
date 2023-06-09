c6a31c7e9babd0390436c471834ef5391ab9bf29
Lucas Vieira <ls6182315@gmail.com>
2023-06-09 14:03:06 +0000

    create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Logo from '../assets/Pigz Gestão - Logo.svg'
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between m-4 text-white-50">
+      <Image src={Logo} alt='pigz-logo' />
+      <button
+        type="submit"
+        className="bg-orange-150 hover:bg-orange-200 text-white font-sans py-2 px-4 rounded-full"
+      >
+        Já sou parceiro
+      </button>
+    </nav>
+  )
+}
