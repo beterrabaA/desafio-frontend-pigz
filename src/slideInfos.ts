f6ffc6ed9d1b6e693cc03c66b4597215ec127b6a
Lucas Vieira <ls6182315@gmail.com>
2023-06-12 20:28:53 +0000

    slide data

---
+++ src/slideInfos.ts
@@ -0,0 +1,8 @@
+export const slideInfos = [
+  {
+    title: 'Pigz Marketplace',
+    description:
+      'Além de uma página exclusiva, com o Pigz Marketplace sua loja terá mais visibilidade, no app e no site. Uma vitrine com milhares de clientes todos os dias, pra vender muito mais.',
+    image: '../assets/',
+  },
+]
