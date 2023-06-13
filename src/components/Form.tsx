45850d3b2c1e1eedd646c1571256a5dc5d12736e
Lucas Vieira <ls6182315@gmail.com>
2023-06-13 11:07:52 +0000

    form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,8 @@
 export default function Form() {
   return (
-    <div className="flex bg-white-50 flex-col w-full font-sans rounded-3xl px-3 py-6">
+    <div className="flex w-full flex-col rounded-3xl bg-white-50 px-3 py-6 font-sans">
       <div>
-        <h2 className="text-3xl font-sans">Quero vender no Pigz</h2>
+        <h2 className="font-sans text-3xl">Quero vender no Pigz</h2>
         <p>Dê o primeiro passo para aumentar suas vendas</p>
       </div>
       <form className="flex flex-col text-left">
@@ -25,12 +25,20 @@ export default function Form() {
         <label htmlFor="phone">Telefone</label>
         <input
           className="rounded-md border-2"
-          type="tel" name="phone" id="phone" placeholder="(95) 99876-5432" />
+          type="tel"
+          name="phone"
+          id="phone"
+          placeholder="(95) 99876-5432"
+        />
       </form>
       <p>
-        Ao continuar, aceito que a Pigz entre em contato comigo por telefone, e-mail ou WhatsApp.
+        Ao continuar, aceito que a Pigz entre em contato comigo por telefone,
+        e-mail ou WhatsApp.
       </p>
-      <button type="submit" className="bg-orange-150 hover:bg-orange-200 text-white-50 font-sans py-2 px-4 rounded-full">
+      <button
+        type="submit"
+        className="rounded-full bg-orange-150 px-4 py-2 font-sans text-white-50 hover:bg-orange-200"
+      >
         Continuar
       </button>
     </div>
