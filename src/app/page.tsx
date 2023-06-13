60df90d09219eb5e159009161af37611e706baa8
Lucas Vieira <ls6182315@gmail.com>
2023-06-13 11:07:47 +0000

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import FrontEntry from "../components/FrontEntry";
-import Navbar from "../components/Navbar";
+import Footer from '../components/Footer'
+import FrontEntry from '../components/FrontEntry'
+import Navbar from '../components/Navbar'
+import SecondEntry from '../components/SecondEntry'
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <FrontEntry />
+      <SecondEntry />
+      <Footer />
     </>
   )
 }
